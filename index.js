04d27a6b895fb3a2437b28f229f826b11261f944
Doron Segal <doron2402@gmail.com>
2015-09-04 00:46:53 -0700

    adding more events

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ if (cluster.isMaster) {
   for (var i =0; i < numCpus; i++){
     cluster.fork();
   }
-
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker %d died (%s). restarting...',
+                worker.process.pid, signal || code);
+                cluster.fork();
+  });
   cluster.on('death', function(worker) {
     console.log('worker ' + worker.pid + ' died');
   });
   cluster.on('listening', function(address) {
-      console.log('Worker is listening: ', address.id);
+    console.log('Worker is listening: ', address.id);
+    console.log("A worker is now connected to " + address.address + ":" + address.port);
+  });
+  cluster.on('disconnect', function(worker) {
+    console.log('The worker #' + worker.id + ' has disconnected');
   });
 }else{
   http.Server(function(req, res){
